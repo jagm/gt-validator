780166849d3f5e71c664a6d3eedddf7d45362d82
jagm <jagiela.mateusz@gmail.com>
2013-10-19 15:33:41 +0200

    add reading data file

---
+++ test_load_data_file.py
@@ -0,0 +1,18 @@
+from unittest import TestCase
+import validator
+
+
+class TestLoad_data_file(TestCase):
+    def test_load_data_file(self):
+        # given
+        example = [
+            'ABC|DEF',
+            '123|456'
+        ]
+
+        # when
+        data = validator.load_data_file('test/test_flat_data.dat')
+
+        #then
+        for i, line in enumerate(data):
+            self.assertEqual(line, example[i])
--- validator.py
+++ validator.py
@@ -8,8 +8,13 @@ def load_configuration(config_file='config.json'):
     return json.load(open(config_file))
 
 
+def load_data_file(data_file):
+    return (line.rstrip('\n') for line in open(data_file, 'r'))
+
+
 def main():
     configuration = load_configuration()
+
 
 if __name__ == '__main__':
     main()
