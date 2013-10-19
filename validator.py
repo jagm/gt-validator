d91d68e9486aa831313c0c3141b2c26417d233d5
jagm <jagiela.mateusz@gmail.com>
2013-10-19 14:55:56 +0200

    add main method

--- validator.py
+++ validator.py
@@ -6,3 +6,10 @@ import json
 
 def load_configuration(config_file='config.json'):
     return json.load(open(config_file))
+
+
+def main():
+    configuration = load_configuration()
+
+if __name__ == '__main__':
+    main()
