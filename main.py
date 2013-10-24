8a538753e8a85b29635ea5c5fb1096070aaf1cc1
jagm <jagiela.mateusz@gmail.com>
2013-10-24 23:44:48 +0200

    add run parameter for configuration file

--- main.py
+++ main.py
@@ -19,12 +19,12 @@ def load_data_file(data_file):
     return (line.rstrip('\n') for line in open(data_file, 'r'))
 
 
-def main(data_file='examples/flat_data.dat'):
+def main(data_file='examples/flat_data.dat', config_file='examples/config.json'):
     configuration = {}
     try:
-        configuration = load_configuration('examples/config.json')
+        configuration = load_configuration(config_file)
     except IOError:
-        logging.error("Cannot open file: %s" % 'examples/config.json')
+        logging.error("Cannot open file: %s" % config_file)
         exit()
 
     data = []
@@ -40,7 +40,10 @@ def main(data_file='examples/flat_data.dat'):
 
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        logging.error("Incorrect arguments: %s. Provide file name to validate" % sys.argv)
-    else:
+    length = len(sys.argv)
+    if length == 2:
         main(sys.argv[1])
+    elif length == 3:
+        main(sys.argv[1], sys.argv[2])
+    else:
+        print "Incorrect arguments: %s. Provide file name to validate" % sys.argv
